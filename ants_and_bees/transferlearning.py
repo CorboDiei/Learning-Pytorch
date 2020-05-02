41663164cf6aa89a30209c437efe222db1ee31cd
David <corbodiei@gmail.com>
2020-05-02 15:19:38 -0500

    created transferlearning

---
+++ ants_and_bees/transferlearning.py
@@ -0,0 +1,115 @@
+"""
+CIFAR 10
+"""
+
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from torch import nn
+import torch.nn.functional as F
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([transforms.Resize((32, 32)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))
+                                ])
+training_dataset = datasets.CIFAR10(root='.\data', train=True, download=True, transform=transform)
+validation_dataset = datasets.CIFAR10(root='.\data', train=False, download=True, transform=transform)
+
+training_loader = torch.utils.data.DataLoader(dataset=training_dataset, batch_size=100, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=100, shuffle=False)
+
+def im_convert(tensor):
+    image = tensor.cpu().clone().detach().numpy()
+    image = image.transpose(1, 2, 0)
+    image = image * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))
+    image = image.clip(0, 1)
+    return image
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+data_iter = iter(training_loader)
+images, labels = data_iter.next()
+fig = plt.figure(figsize=(25, 4))
+
+for i in np.arange(20):
+    ax = fig.add_subplot(2, 10, i + 1, xticks=[], yticks=[])
+    plt.imshow(im_convert(images[i]))
+    ax.set_title(classes[labels[i].item()])
+    
+class LeNet(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(1250, 500)
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 1250)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+     #change
+model = LeNet().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+epochs = 12
+running_loss_history = []
+running_corrects_history = []
+val_running_loss_history = []
+val_running_loss_corrects = []
+
+for e in range(epochs):
+    running_loss = 0.0
+    running_corrects = 0.0
+    val_running_loss = 0.0
+    val_running_corrects = 0.0
+    
+    for inputs, labels, in training_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        _, preds = torch.max(outputs, 1)
+        running_corrects += torch.sum(preds == labels.data)
+        
+        running_loss += loss.item()
+    else:
+        with torch.no_grad():
+            for val_inputs, val_labels in validation_loader:
+                val_inputs = val_inputs.to(device)
+                val_labels = val_labels.to(device)
+                val_outputs = model(val_inputs)
+                val_loss = criterion(val_outputs, val_labels)
+                _, val_preds = torch.max(val_outputs, 1)
+                val_running_corrects += torch.sum(val_preds == val_labels.data)
+                val_running_loss += val_loss.item()
+            
+            
+        val_epoch_loss = val_running_loss/len(validation_loader)
+        val_epoch_accuracy = val_running_corrects.float()/len(validation_loader)
+#        running_corrects_history.append(epoch_accuracy)
+#        running_loss_history.append(epoch_loss)
+        val_running_loss_history.append(val_epoch_loss)
+        val_running_loss_corrects.append(val_epoch_accuracy)
+        
+        print('epoch: ', e + 1)
+        print('validation loss: {:.4f}, validation accuracy {:.4f}'.format(val_epoch_loss, val_epoch_accuracy.item()))
+#        
+        
+        

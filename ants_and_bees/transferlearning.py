2025f6b81201968d3788cdb27e13fcdbe3298b6f
David <corbodiei@gmail.com>
2020-05-02 16:18:18 -0500

    finished transferlearning

--- ants_and_bees/transferlearning.py
+++ ants_and_bees/transferlearning.py
@@ -1,25 +1,33 @@
 """
-CIFAR 10
+Transfer Learning
 """
-
 import torch
-from torchvision import datasets, transforms
-import numpy as np
 import matplotlib.pyplot as plt
-from torch import nn
+import numpy as np
 import torch.nn.functional as F
+import torch.nn as nn
+from torchvision import datasets, transforms, models
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-transform = transforms.Compose([transforms.Resize((32, 32)),
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.5,), (0.5,))
-                                ])
-training_dataset = datasets.CIFAR10(root='.\data', train=True, download=True, transform=transform)
-validation_dataset = datasets.CIFAR10(root='.\data', train=False, download=True, transform=transform)
-
-training_loader = torch.utils.data.DataLoader(dataset=training_dataset, batch_size=100, shuffle=True)
-validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=100, shuffle=False)
+transform_train = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.RandomAffine(0, shear=10, scale=(0.8,1.2)),
+                                      transforms.ColorJitter(brightness=1, contrast=1, saturation=1),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                               ])
+ 
+ 
+transform = transforms.Compose([transforms.Resize((224, 224)),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                               ])
+training_dataset = datasets.ImageFolder(root='train', transform=transform_train)
+validation_dataset = datasets.ImageFolder(root='val', transform=transform)
+ 
+training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=20, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=20, shuffle=True)
 
 def im_convert(tensor):
     image = tensor.cpu().clone().detach().numpy()
@@ -28,54 +36,44 @@ def im_convert(tensor):
     image = image.clip(0, 1)
     return image
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('ant', 'bee')
 
-data_iter = iter(training_loader)
-images, labels = data_iter.next()
+dataiter = iter(training_loader)
+images, labels = dataiter.next()
 fig = plt.figure(figsize=(25, 4))
+ 
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 10, idx+1, xticks=[], yticks=[])
+    plt.imshow(im_convert(images[idx]))
+    ax.set_title(classes[labels[idx].item()])
+  
+model = models.vgg16(pretrained=True)
 
-for i in np.arange(20):
-    ax = fig.add_subplot(2, 10, i + 1, xticks=[], yticks=[])
-    plt.imshow(im_convert(images[i]))
-    ax.set_title(classes[labels[i].item()])
+for param in model.features.parameters():
+    param.requires_grad = False
     
-class LeNet(nn.Module):
-    def __init__(self, ):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(1250, 500)
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(500, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 1250)
-        x = F.relu(self.fc1(x))
-        x = self.dropout1(x)
-        x = self.fc2(x)
-        return x
-     #change
-model = LeNet().to(device)
+n_inputs = model.classifier[6].in_features
+last_layer = nn.Linear(n_inputs, len(classes))
+model.classifier[6] = last_layer
+model.to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-epochs = 12
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
+
+epochs = 5
 running_loss_history = []
 running_corrects_history = []
 val_running_loss_history = []
-val_running_loss_corrects = []
-
+val_running_corrects_history = []
+ 
 for e in range(epochs):
+  
     running_loss = 0.0
     running_corrects = 0.0
     val_running_loss = 0.0
     val_running_corrects = 0.0
-    
-    for inputs, labels, in training_loader:
+  
+    for inputs, labels in training_loader:
         inputs = inputs.to(device)
         labels = labels.to(device)
         outputs = model(inputs)
@@ -84,11 +82,11 @@ for e in range(epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+    
         _, preds = torch.max(outputs, 1)
-        running_corrects += torch.sum(preds == labels.data)
-        
         running_loss += loss.item()
+        running_corrects += torch.sum(preds == labels.data)
+ 
     else:
         with torch.no_grad():
             for val_inputs, val_labels in validation_loader:
@@ -96,20 +94,22 @@ for e in range(epochs):
                 val_labels = val_labels.to(device)
                 val_outputs = model(val_inputs)
                 val_loss = criterion(val_outputs, val_labels)
-                _, val_preds = torch.max(val_outputs, 1)
-                val_running_corrects += torch.sum(val_preds == val_labels.data)
-                val_running_loss += val_loss.item()
-            
-            
-        val_epoch_loss = val_running_loss/len(validation_loader)
-        val_epoch_accuracy = val_running_corrects.float()/len(validation_loader)
-#        running_corrects_history.append(epoch_accuracy)
-#        running_loss_history.append(epoch_loss)
+        
+            _, val_preds = torch.max(val_outputs, 1)
+            val_running_loss += val_loss.item()
+            val_running_corrects += torch.sum(val_preds == val_labels.data)
+      
+        epoch_loss = running_loss/len(training_loader.dataset)
+        epoch_acc = running_corrects.float()/ len(training_loader.dataset)
+        running_loss_history.append(epoch_loss)
+        running_corrects_history.append(epoch_acc)
+    
+        val_epoch_loss = val_running_loss/len(validation_loader.dataset)
+        val_epoch_acc = val_running_corrects.float()/ len(validation_loader.dataset)
         val_running_loss_history.append(val_epoch_loss)
-        val_running_loss_corrects.append(val_epoch_accuracy)
-        
-        print('epoch: ', e + 1)
-        print('validation loss: {:.4f}, validation accuracy {:.4f}'.format(val_epoch_loss, val_epoch_accuracy.item()))
-#        
-        
+        val_running_corrects_history.append(val_epoch_acc)
+        print('epoch :', (e+1))
+        print('training loss: {:.4f}, acc {:.4f} '.format(epoch_loss, epoch_acc.item()))
+        print('validation loss: {:.4f}, validation acc {:.4f} '.format(val_epoch_loss, val_epoch_acc.item()))
+            
         
